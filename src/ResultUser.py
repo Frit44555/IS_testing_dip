64913b17c02fc7d3677f797d4718b37e438ca01d
Frit44555 <sosin.niki@mail.ru>
2023-05-27 18:46:23 +0300

    Классы просмотра результата

---
+++ src/ResultUser.py
@@ -0,0 +1,26 @@
+from UI import Ui_ResultUser
+
+
+class ResultUser(Ui_ResultUser):
+    def __init__(self, user_id, data_base, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+        # Переменные________________________________
+        self.__user = user_id
+        self.__db = data_base
+        # ________________________________
+
+    def __filling_result_list(self):
+        """
+        Заполняет список пройденных тестов.
+        :return None
+        """
+        pass
+
+    def __draw_statistics(self):
+        """
+        Показывает статистику результата
+        :return None
+        """
+        pass
