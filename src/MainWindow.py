3d19bb167a68dd4b666076737bc8f9496021f80b
Frit44555 <sosin.niki@mail.ru>
2023-05-27 18:44:50 +0300

    Добавил: главное меню (класс MenuBar), всплывающее окно "О программе" (функция __about_program), функцию повторной аутентификации (__authentication_again), заголовок функции open_lesson, и написал docstrings

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,8 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtCore import pyqtSlot
 from UI.Ui_MainWindow import Ui_MainWindow
 from Authentication import Authentication
 from ListSearch import ListSearch
 from Lesson import Lesson
+from MainMenu import MainMenu
+import Words as wrd
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -12,44 +15,132 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # Переменные________________________________
         self.__db = data_base
-        self.__user = None
         self.__list_search = None
         self.__lesson = None
+        self.__authentication = None
+
+        # Картеж
+        # Для пользователя (ID пользователя, ID группы, логин, имя, фамилия, отчество, дату регистрации, примечания)
+        # Для администратора (ID администратора, логин, имя, фамилия, отчество, примечания)
+        self.__user = None
         # ________________________________
 
-        # Функции________________________________
-        self.authentication = Authentication(main_window=self, data_base=self.__db)
-        self.work_vertical_layout.addWidget(self.authentication)
+        # Объекты________________________________
+        self.__authentication = Authentication(main_window=self, data_base=self.__db)
+        self.work_vertical_layout.addWidget(self.__authentication)
         # ________________________________
 
         # Опции________________________________
         self.to_main_push_button.setEnabled(False)
         self.exit_push_button.setEnabled(False)
-        self.update_push_button.setEnabled(False)
+        self.refresh_push_button.setEnabled(False)
+        # ________________________________
+
+        # MenuBar________________________________
+        main_menu = MainMenu(parent=self)
+        self.setMenuBar(main_menu)
+        main_menu.about.triggered.connect(self.__about_program)
         # ________________________________
 
     def __set_action(self):
+        """
+        Устанавливает состояния и действия виджетам.
+        :return None
+        """
+        # Опции________________________________
         self.to_main_push_button.setEnabled(True)
         self.exit_push_button.setEnabled(True)
-        self.update_push_button.setEnabled(True)
-        self.to_main_push_button.clicked.connect(self.destroy_active_widget)
+        self.refresh_push_button.setEnabled(True)
+        # ________________________________
 
+        # Действия________________________________
+        self.to_main_push_button.clicked.connect(self.__to_main_page)
+        self.refresh_push_button.clicked.connect(self.__refresh_data)
+        self.exit_push_button.clicked.connect(self.__authentication_again)
+        # ________________________________
+
+    @pyqtSlot()
     def __to_main_page(self):
-        if self.__list_search:
+        """
+        Выполняет переход к главному виджету, в зависимости от авторизированного пользователя.
+        :return None
+        """
+        try:
+            if self.__lesson:
+                self.__destroy_active_widget(self.__lesson)
             self.__list_search.show()
-            self.__destroy_active_widget()
+        except BaseException as BE:
+            print('FATAL ERROR:', BE)
 
-    def destroy_active_widget(self):
+    @pyqtSlot()
+    def __refresh_data(self):
+        """
+        Обновляет данные таблиц и списков главных виджетов.
+        :return None
+        """
+        try:
+            self.__destroy_active_widget(self.__list_search)
+            del self.__list_search
+            self.__list_search = ListSearch(main_window=self, data_base=self.__db, group_user=self.__user[1])
+            self.work_vertical_layout.addWidget(self.__list_search)
+        except BaseException as BE:
+            print('EXECUTION ERROR:', BE)
+
+
+    @pyqtSlot()
+    def __authentication_again(self):
+        """
+        Переход в аутентификации из рабочей среды. Закрывает все рабочие виджеты в зависимости от
+        авторизированного пользователя.
+        :return None
+        """
+        # Опции________________________________
+        self.to_main_push_button.setEnabled(False)
+        self.exit_push_button.setEnabled(False)
+        self.refresh_push_button.setEnabled(False)
+        # ________________________________
+
+        self.__user = None
+        if self.__list_search:
+            self.__destroy_active_widget(self.__list_search)
         if self.__lesson:
-            try:
-                self.__lesson.setParent(None)
-                self.__list_search.show()
-            except BaseException as BE:
-                print('FATAL ERROR:', BE)
+            self.__destroy_active_widget(self.__lesson)
+        if self.__authentication:
+            self.__destroy_active_widget(self.__authentication)
+
+        self.__authentication = Authentication(main_window=self, data_base=self.__db)
+        self.work_vertical_layout.addWidget(self.__authentication)
+
+    @pyqtSlot()
+    def __about_program(self):
+        """
+        Показывает сведения о разработанной системе.
+        :return None
+        """
+        QMessageBox.about(self, wrd.main_menu['about_program'], wrd.main_menu['about_program_action_text'])
+
+    def __destroy_active_widget(self, active_widget):
+        """
+        Закрывает переданный виджет.
+        :return None
+        """
+        active_widget.setParent(None)
+        if isinstance(active_widget, Lesson):
+            self.__lesson = None
+        elif isinstance(active_widget, ListSearch):
+            self.__list_search = None
+        elif isinstance(active_widget, Authentication):
+            self.__authentication = None
 
     def put_form_for_user(self, user):
+        """
+        Заполнение главного окна, виджетами для пользователя.
+        :param user:
+        :return None
+        """
         try:
-            del self.authentication
+            # Закрытие объекта аутентификации
+            self.__destroy_active_widget(self.__authentication)
             self.__user = user
             self.__set_action()
             self.__list_search = ListSearch(main_window=self, data_base=self.__db, group_user=user[1])
@@ -58,18 +149,28 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             print('FATAL ERROR:', BE)
 
     def put_form_for_admin(self, user):
+        """
+        Заполнение главного окна, для администратора.
+        :param user:
+        :return None
+        """
         try:
-            del self.authentication
+            # Закрытие объекта аутентификации
+            self.__destroy_active_widget(self.__authentication)
             self.__user = user
             self.__set_action()
         except BaseException as BE:
             print('FATAL ERROR:', BE)
 
     def open_lesson(self, lesson_id):
+        """
+        Открывает виджет изучения учебного материала
+        :param lesson_id:
+        :return None
+        """
         # ПЕРЕДАЙ ID МАТЕРИАЛА А НЕ ГРУППЫ
-        try:
-            self.__lesson = Lesson(data_base=self.__db, lesson_id=lesson_id)
-            self.work_vertical_layout.addWidget(self.__lesson)
-        except BaseException as BE:
-            print('FATAL ERROR:', BE)
+        self.__lesson = Lesson(data_base=self.__db, lesson_id=lesson_id)
+        self.work_vertical_layout.addWidget(self.__lesson)
 
+    def open_testing(self):
+        pass
