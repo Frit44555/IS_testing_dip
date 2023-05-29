2f2017700691a6592fbd8f686c1a146e9e544f04
Frit44555 <sosin.niki@mail.ru>
2023-05-29 19:59:48 +0300

    Исправил ошибочно названную переменную __correctness_widget → __status_answer_widget. Добавил документацию

--- src/quest/QuestOneAnswer.py
+++ src/quest/QuestOneAnswer.py
@@ -24,25 +24,39 @@ class QuestOneAnswer(QWidget, Ui_QuestOneAnswer):
         # ________________________________
 
         # Объекты________________________________
-        self.__correctness_widget = StatusAnswerWidget(self)
-        self.verticalLayout.addWidget(self.__correctness_widget)
+        self.__status_answer_widget = StatusAnswerWidget(self)
+        self.verticalLayout.addWidget(self.__status_answer_widget)
         # ________________________________
 
     def __set_actions(self):
+        """
+        Устанавливает действия кнопкам
+        :return None
+        """
         self.confirm_push_button.clicked.connect(self.__answered)
 
     @pyqtSlot()
     def __answered(self):
+        """
+        При нажатии на кнопку ответить запускается эта функция.
+        Вызывает функцию отправки и меняет статус ответа на "Отвеченный"
+        Проверяет, отвечал ли пользователь или нет, в случае если не отвечал, то предупредит об этом.
+        :return None
+        """
         if not self.answer1_radio_button.isChecked() \
                 and not self.answer2_radio_button.isChecked() \
                 and not self.answer3_radio_button.isChecked() \
                 and not self.answer4_radio_button.isChecked():
             QMessageBox.about(self, wrd.testing_quest['title'], wrd.testing_quest['text'])
         else:
-            self.__correctness_widget.answered()
+            self.__status_answer_widget.answered()
             self.__send_answer()
 
     def __fill_quest(self):
+        """
+        Заполнение виджета данными. Заполняется вопрос, картинка и четыре ответа.
+        :return None
+        """
         # Таблица вопросов хранит: ID вопроса, тип вопроса, балл, картинка, вопрос,
         # ответ1, ответ2, ответ3, ответ4, правильный ответ.
         self.question_text_browser.setText(self.__data[4])
@@ -58,6 +72,10 @@ class QuestOneAnswer(QWidget, Ui_QuestOneAnswer):
         self.answer4_text_browser.setText(self.__data[random_list[3]])
 
     def __check_answer(self):
+        """
+        Функция проверяет, какой ответ был выбран.
+        :return boolean
+        """
         answer = ''
         if self.answer1_radio_button.isChecked():
             answer = self.answer1_text_browser.toPlainText()
@@ -74,9 +92,10 @@ class QuestOneAnswer(QWidget, Ui_QuestOneAnswer):
             return answer, False
 
     def __send_answer(self):
+        """
+        Функция отправляет виджету Testing выбранный ответ.
+        :return None
+        """
         answer_id = self.__data[0]
         answer, correct = self.__check_answer()
         self.__testing.add_answer(answer_id, answer, correct)
-
-
-
