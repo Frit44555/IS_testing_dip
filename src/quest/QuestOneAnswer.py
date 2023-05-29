a5092f8f74d7ed67d8e96922f1ebaa864923ffaa
Frit44555 <sosin.niki@mail.ru>
2023-05-29 18:42:36 +0300

    Реализовал класс QuestOneAnswer

--- src/quest/QuestOneAnswer.py
+++ src/quest/QuestOneAnswer.py
@@ -1,10 +1,82 @@
-from UI import ui_quest
+from PyQt5.QtWidgets import QWidget, QMessageBox
+from PyQt5.QtCore import pyqtSlot
+import random
+
+# My widgets
+from UI.ui_quest.Ui_QuestOneAnswer import Ui_QuestOneAnswer
+from src.quest.СorrectnessWidget import CorrectnessWidget
+import src.Words as wrd
 
 
-class QuestOneAnswer(ui_quest.Ui_QuestOneAnswer):
-    def __init__(self, parent=None):
+class QuestOneAnswer(QWidget, Ui_QuestOneAnswer):
+    def __init__(self, data, parent=None):
         super().__init__(parent)
         self.setupUi(self)
 
-    def answered(self):
-        pass
+        # Переменные________________________________
+        self.__data = data
+        self.__testing = parent
+        # ________________________________
+
+        # Функции________________________________
+        self.__fill_quest()
+        self.__set_actions()
+        # ________________________________
+
+        # Объекты________________________________
+        self.__correctness_widget = CorrectnessWidget(self)
+        self.verticalLayout.addWidget(self.__correctness_widget)
+        # ________________________________
+
+    def __set_actions(self):
+        self.confirm_push_button.clicked.connect(self.__answered)
+
+    @pyqtSlot()
+    def __answered(self):
+        if not self.answer1_radio_button.isChecked() \
+                and not self.answer2_radio_button.isChecked() \
+                and not self.answer3_radio_button.isChecked() \
+                and not self.answer4_radio_button.isChecked():
+            QMessageBox.about(self, wrd.testing_quest['title'], wrd.testing_quest['text'])
+        else:
+            self.__correctness_widget.answered()
+            self.__send_answer()
+
+    def __fill_quest(self):
+        # Таблица вопросов хранит: ID вопроса, тип вопроса, балл, картинка, вопрос,
+        # ответ1, ответ2, ответ3, ответ4, правильный ответ.
+        self.question_text_browser.setText(self.__data[4])
+        if self.__data[3]:
+            self.picture_label.setPixmap()
+
+        # случайное заполнение ответов
+        random_list = [5, 6, 7, 8]
+        random.shuffle(random_list)
+        self.answer1_text_browser.setText(self.__data[random_list[0]])
+        self.answer2_text_browser.setText(self.__data[random_list[1]])
+        self.answer3_text_browser.setText(self.__data[random_list[2]])
+        self.answer4_text_browser.setText(self.__data[random_list[3]])
+
+    def __check_answer(self):
+        answer = ''
+        if self.answer1_radio_button.isChecked():
+            answer = self.answer1_text_browser.toPlainText()
+        elif self.answer2_radio_button.isChecked():
+            answer = self.answer2_text_browser.toPlainText()
+        elif self.answer3_radio_button.isChecked():
+            answer = self.answer3_text_browser.toPlainText()
+        elif self.answer4_radio_button.isChecked():
+            answer = self.answer4_text_browser.toPlainText()
+
+        if answer == self.__data[9]:
+            return answer, True
+        else:
+            return answer, False
+
+    def __send_answer(self):
+        answer_id = self.__data[0]
+        answer, correct = self.__check_answer()
+        self.__testing.add_answer(answer_id, answer, correct)
+
+
+
