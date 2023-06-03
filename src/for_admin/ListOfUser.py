5d02680bf4d944dc536d91335f07130d66003b0f
Frit44555 <sosin.niki@mail.ru>
2023-06-03 21:59:34 +0300

    Добавил функции и объекты что-то вроде диалоговых окон, для взаимодействия со списками пользователей, тегов и групп

--- src/for_admin/ListOfUser.py
+++ src/for_admin/ListOfUser.py
@@ -1,6 +1,13 @@
 from PyQt5.QtWidgets import QWidget
 from UI.form_for_admin.Ui_ListOfUser import Ui_ListOfUser
 from psycopg2 import Error
+from PyQt5.QtCore import pyqtSlot
+
+# My widgets
+from src.for_admin.dialog_admin.CreateTagDialog import CreateTagDialog
+from src.for_admin.dialog_admin.ChangeUserGroup import ChangeUserGroup
+from src.for_admin.dialog_admin.CreateGroup import CreateGroup
+from src.for_admin.dialog_admin.ChangeGroup import ChangeGroup
 
 
 class ListOfUser(QWidget, Ui_ListOfUser):
@@ -13,13 +20,35 @@ class ListOfUser(QWidget, Ui_ListOfUser):
         self.__list_users = None
         self.__groups_user = None
         self.__tags_user = None
+
+        self.__current_user = None
+        self.__current_group = None
+        self.__current_tag = None
         # ________________________________
 
-        # Переменные________________________________
+        # Функции________________________________
         self.__fill_user_list()
         self.__fill_group_list()
         self.__fill_tag_list()
+        self.__set_action()
         # ________________________________
+
+        # Опции________________________________
+        self.change_group_push_button.setEnabled(False)
+        self.change_tags_group_push_button.setEnabled(False)
+        self.delete_user_push_button.setEnabled(False)
+        self.delete_group_push_button.setEnabled(False)
+        self.delete_tag_push_button.setEnabled(False)
+        # ________________________________
+
+    def __set_action(self):
+        self.create_tag_push_button.clicked.connect(self.__create_tag)
+        self.create_group_push_button.clicked.connect(self.__create_group)
+        self.change_group_push_button.clicked.connect(self.__change_user_group)
+        self.change_tags_group_push_button.clicked.connect(self.__change_group)
+        self.users_list_widget.clicked.connect(self.__get_user)
+        self.groups_user_list_widget.clicked.connect(self.__get_group)
+        self.tags_list_widget.clicked.connect(self.__get_tags)
 
     def __fill_user_list(self):
         """
@@ -61,7 +90,6 @@ class ListOfUser(QWidget, Ui_ListOfUser):
 
             self.groups_user_list_widget.addItem(elem_lest)
 
-
     def __fill_tag_list(self):
         try:
             self.__tags_user = self.__db.get_tags()
@@ -78,3 +106,40 @@ class ListOfUser(QWidget, Ui_ListOfUser):
             elem_lest += 'Название: ' + row[1] + ' | '
 
             self.tags_list_widget.addItem(elem_lest)
+
+    @pyqtSlot()
+    def __create_tag(self):
+        self.__dialog_tag = CreateTagDialog(data_base=self.__db)
+        self.__dialog_tag.show()
+
+    @pyqtSlot()
+    def __create_group(self):
+        self.__dialog_group = CreateGroup(data_base=self.__db)
+        self.__dialog_group.show()
+
+    @pyqtSlot()
+    def __change_user_group(self):
+        self.__dialog_user_group = ChangeUserGroup(data_base=self.__db, data=self.__current_user)
+        self.__dialog_user_group.show()
+
+    @pyqtSlot()
+    def __change_group(self):
+        self.__dialog_user_group = ChangeGroup(data_base=self.__db, data=self.__current_group)
+        self.__dialog_user_group.show()
+
+    def __get_user(self):
+        self.change_group_push_button.setEnabled(True)
+        self.delete_user_push_button.setEnabled(True)
+        index = self.users_list_widget.selectedIndexes()[0].row()
+        self.__current_user = self.__list_users[index]
+
+    def __get_group(self):
+        self.change_tags_group_push_button.setEnabled(True)
+        self.delete_group_push_button.setEnabled(True)
+        index = self.groups_user_list_widget.selectedIndexes()[0].row()
+        self.__current_group = self.__groups_user[index]
+
+    def __get_tags(self):
+        self.delete_tag_push_button.setEnabled(True)
+        index = self.tags_list_widget.selectedIndexes()[0].row()
+        self.__current_tag = self.__tags_user[index]
