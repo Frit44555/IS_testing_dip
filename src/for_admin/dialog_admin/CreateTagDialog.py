555b0ae20eb291d554cc28734849e17dbc5f0873
Frit44555 <sosin.niki@mail.ru>
2023-06-04 16:07:28 +0300

    Реализовал класс добавления тега

--- src/for_admin/dialog_admin/CreateTagDialog.py
+++ src/for_admin/dialog_admin/CreateTagDialog.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget
 from UI.form_for_admin.dialog_admin.Ui_CreateTagDialog import Ui_Ui_CreateTagDialog
+from psycopg2 import Error
 
 
 class CreateTagDialog(QWidget, Ui_Ui_CreateTagDialog):
@@ -20,9 +21,26 @@ class CreateTagDialog(QWidget, Ui_Ui_CreateTagDialog):
         # ________________________________
 
     def __set_action(self):
+        """
+        Устанавливает действия кнопкам
+        """
         self.cancel_push_button.clicked.connect(self.close)
         self.name_line_edit.textChanged.connect(self.__set_enabled_button)
+        self.create_tag_push_button.clicked.connect(self.__create_tag)
 
     def __set_enabled_button(self):
+        """
+        Делает кнопку "создать" активной
+        """
         self.create_tag_push_button.setEnabled(True)
 
+    def __create_tag(self):
+        """
+        Метод создаёт тег в БД
+        """
+        try:
+            self.__db.create_tag(self.name_line_edit.text())
+        except (Exception, Error) as error:
+            print('ERROR QUERY:', error)
+        self.__db.connection.commit()
+        self.close()
