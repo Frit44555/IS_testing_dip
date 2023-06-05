c2bcf772dc69f763d50a29e27438f0e8e90243ab
Frit44555 <sosin.niki@mail.ru>
2023-06-05 20:56:09 +0300

    Реализовал заполнение списков

--- src/for_admin/Analysis.py
+++ src/for_admin/Analysis.py
@@ -14,6 +14,7 @@ class Analysis(QWidget, Ui_Analysis):
 
         # Функции________________________________
         self.__fill_list_users()
+        self.__fill_list_test()
         # ________________________________
 
     def __set_action(self):
@@ -38,17 +39,36 @@ class Analysis(QWidget, Ui_Analysis):
         for row in self.__results_all:
             # ID результата, ID пользователя, ID теста, массив ответов, время начала, время конца, коммент админа,
             # коммент пользователя, статус проверки
-            elem_lest = ''
-            elem_lest += 'Номер: ' + str(row[0]) + ' | '
-            elem_lest += 'ID пользователя: ' + str(row[1]) + ' | '
-            elem_lest += 'Тест: ' + str(row[2]) + ' | '
-            elem_lest += 'Начал: ' + str(row[4])[:-7] + ' | '
-            elem_lest += 'Закончил: ' + str(row[5])[:-7] + ' | '
+            elem_list = ''
+            elem_list += 'Номер: ' + str(row[0]) + ' | '
+            elem_list += 'ID пользователя: ' + str(row[1]) + ' | '
+            elem_list += 'Тест: ' + str(row[2]) + ' | '
+            elem_list += 'Начал: ' + str(row[4])[:-7] + ' | '
+            elem_list += 'Закончил: ' + str(row[5])[:-7] + ' | '
 
-            self.result_user_list_widget.addItem(elem_lest)
+            self.result_user_list_widget.addItem(elem_list)
 
     def __fill_list_test(self):
         """
         Заполняет список статистики тестов
         """
-        pass
+        try:
+            self.__tests_all = self.__db.ger_tests_all()
+        except (Exception, Error) as error:
+            print('ERROR QUERY:', error)
+
+        if self.__tests_all == 1:
+            return
+
+        self.result_test_list_widget.clear()
+        for row in self.__tests_all:
+            # ID теста, массив вопросов, ID тега, название, тип, количество тестов, время выполнения, примечание
+            elem_list = ''
+            elem_list += 'Тест №: ' + str(row[0]) + ' | '
+            elem_list += 'Тег №: ' + str(row[2]) + ' | '
+            elem_list += 'Название: ' + row[3] + ' | '
+            elem_list += 'Тип: ' + row[4] + ' | '
+            elem_list += 'Время: ' + str(row[6]) + ' | '
+            elem_list += 'Вопросов: ' + str(row[5]) + ' | '
+
+            self.result_test_list_widget.addItem(elem_list)
