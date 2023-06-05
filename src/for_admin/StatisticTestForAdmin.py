24b0a5300e2a64aa70690afe93aca7854c5037ff
Frit44555 <sosin.niki@mail.ru>
2023-06-05 21:08:14 +0300

    Класс StatisticTestForAdmin и виджет для него

---
+++ src/for_admin/StatisticTestForAdmin.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QWidget
+from UI.form_for_admin.Ui_StatisticTestForAdmin import Ui_StatisticTestForAdmin
+
+
+class StatisticTestForAdmin(QWidget, Ui_StatisticTestForAdmin):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
