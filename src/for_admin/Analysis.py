516a6ba5a518768e5afeb1537175bbde2abc2a76
Frit44555 <sosin.niki@mail.ru>
2023-06-06 20:07:55 +0300

    Добавил вывод виджета статистики

--- src/for_admin/Analysis.py
+++ src/for_admin/Analysis.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget
 from UI.form_for_admin.Ui_Analysis import Ui_Analysis
 from psycopg2 import Error
+import numpy as np
 
 from src.for_admin.StatisticTestForAdmin import StatisticTestForAdmin
 
@@ -12,6 +13,7 @@ class Analysis(QWidget, Ui_Analysis):
 
         # Переменные________________________________
         self.__db = data_base
+        self.__statistic_test = None
         # ________________________________
 
         # Функции________________________________
@@ -77,6 +79,67 @@ class Analysis(QWidget, Ui_Analysis):
             self.result_test_list_widget.addItem(elem_list)
 
     def __show_statistic_test(self):
-        print(1)
-        self.__statistic_test = StatisticTestForAdmin(parent=self)
-        self.vertical_layout_graph_result_test.addWidget(self.__statistic_test)
+        # Item выбранного элемента
+        index = self.result_test_list_widget.selectedIndexes()[0].row()
+        test = self.__tests_all[index]
+        data = self.__calculation_statistic_test(test)
+
+        if not self.__statistic_test:
+            self.__statistic_test = StatisticTestForAdmin(parent=self, data=data)
+            self.vertical_layout_graph_result_test.addWidget(self.__statistic_test)
+        else:
+            self.__statistic_test.setParent(None)
+            self.__statistic_test = None
+            self.__statistic_test = StatisticTestForAdmin(parent=self, data=data)
+            self.vertical_layout_graph_result_test.addWidget(self.__statistic_test)
+
+    def __calculation_statistic_test(self, test):
+        try:
+            test_id = test[0]
+            test_name = test[3]
+            quantity_quests = len(test[1])
+            quantity_quests_in_testing = test[5]
+
+            try:
+                all_bals_on_test = self.__db.get_sorted_questions(test_id)
+                middle_score = self.__db.get_middle_bals_on_test(test_id)
+            except (Exception, Error) as error:
+                print('ERROR QUERY:', error)
+
+            dict_value = dict()
+            key = 0
+            for row in all_bals_on_test:
+                if row[0] in dict_value:
+                    dict_value[row[0]] = dict_value[row[0]] + [row[-2] if row[-1] else 0, ]
+                else:
+                    dict_value[row[0]] = [row[-2] if row[-1] else 0, ]
+                    key = row[0]
+
+            general_variance = np.var(all_bals_on_test)
+            quest_variance = 0
+            general_selection = len(dict_value[key])
+            difficulty = []
+            middle_score = []
+            for lst in dict_value.values():
+                quest_variance += np.var(lst)
+                difficulty.append((len(lst) - lst.count(0)) / general_selection)
+                temp_true = [i for i in lst if i]
+                middle_score.append(sum(temp_true) / len(temp_true))
+
+            reliability = quantity_quests_in_testing / (quantity_quests_in_testing - 1) * \
+                          (1 - (quest_variance / general_variance))
+
+            midle_difficulty_test = sum(difficulty) / len(difficulty)
+
+            difference = dict()
+            for i in range(len(difficulty)):
+
+
+
+
+
+            return test_name, quantity_quests, quantity_quests_in_testing, str(round(middle_score[2], 2)), reliability
+        except BaseException as be:
+            print(be)
+
+
--- src/for_admin/StatisticTestForAdmin.py
+++ src/for_admin/StatisticTestForAdmin.py
@@ -3,6 +3,6 @@ from UI.form_for_admin.Ui_StatisticTestForAdmin import Ui_StatisticTestForAdmin
 
 
 class StatisticTestForAdmin(QWidget, Ui_StatisticTestForAdmin):
-    def __init__(self, parent=None):
+    def __init__(self, data, parent=None):
         super().__init__(parent)
         self.setupUi(self)
